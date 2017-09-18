b3ca3582d0f41d5322150195cb0ef0f0d7c28fdf
Myrfion <32075241+Myrfion@users.noreply.github.com>
2017-09-18 22:25:24 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 
 	$output = json_decode(file_get_contents('php://input'),true);
-	$id = $output['message']['chat']['id'];
+	$id = $output['message']['text'];
 	file_put_contents("logs.txt", $id);
