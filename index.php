162990b3d4c80fcb31ebbd09b2fe527cc97eec85
Myrfion <32075241+Myrfion@users.noreply.github.com>
2017-09-18 22:33:05 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,5 +2,5 @@
 
 	$output = json_decode(file_get_contents('php://input'),true);
 	$id = $output['message']['chat']['id'];
-	file_put_contents("https://api.telegram.org/bot408340680:AAG4FI5TDk45wzuSzib4oQpgCGqGFVCoWBo/sendMessage?chat_id" . $id."&text=pishov nahiu" );
+	file_put_contents("https://api.telegram.org/bot408340680:AAG4FI5TDk45wzuSzib4oQpgCGqGFVCoWBo/sendMessage?chat_id=".$id."&text=pishov nahiu" );
 	file_put_contents("logs.txt", $id);
