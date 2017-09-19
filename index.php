552030385845067e03724fbf1a4e747c63615c6a
Myrfion <32075241+Myrfion@users.noreply.github.com>
2017-09-19 20:47:49 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,5 +2,14 @@
 
 	$output = json_decode(file_get_contents('php://input'),true);
 	$id = $output['message']['chat']['id'];
-	file_get_contents("https://api.telegram.org/bot408340680:AAG4FI5TDk45wzuSzib4oQpgCGqGFVCoWBo/sendMessage?chat_id=".$id."&text=pishov nahiu" );
+	$text = $output['message']['text'];
+	if($text == "hello")
+	{
+		file_get_contents("https://api.telegram.org/bot408340680:AAG4FI5TDk45wzuSzib4oQpgCGqGFVCoWBo/sendMessage?chat_id=".$id."&text=hello" );
+	}
+	else
+	{
+		file_get_contents("https://api.telegram.org/bot408340680:AAG4FI5TDk45wzuSzib4oQpgCGqGFVCoWBo/sendMessage?chat_id=".$id."&text=go to hell" );
+	}
+	
 	file_put_contents("logs.txt", $id);
